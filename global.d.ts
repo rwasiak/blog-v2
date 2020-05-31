c7e299935c97037a648ea510d72f45c23bf24d7d
rw <remigiusz.wasiak@gmail.com>
2020-05-31 14:22:42 +0200

    feat: jest and react-svgr added

--- global.d.ts
+++ global.d.ts
@@ -1,24 +1,9 @@
-declare module '*.png' {
-  const value: any;
-  export default value;
-}
-
-declare module '*.jpg' {
-  const value: any;
-  export default value;
-}
-
-declare module '*.jpeg' {
-  const value: any;
-  export default value;
-}
-
-declare module '*.gif' {
-  const value: any;
-  export default value;
-}
-
 declare module '*.svg' {
+  import * as React from 'react';
+
   const value: any;
   export default value;
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>;
 }
